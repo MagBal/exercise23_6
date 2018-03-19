5ac014af1a686f0999c530978bd09a5ad63ec7e8
Magda Balun <magi@op.pl>
2018-03-19 09:37:24 +0100

    Add funcionality to kanban

---
+++ client/modules/Kanban/Kanban.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Lanes from '../Lane/Lanes';
+import styles from './Kanban.css';
+import { createLaneRequest, fetchLanes } from '../Lane/LaneActions';
+
+const Kanban = (props) => (
+  <div>
+    <button className={styles.AddLane} onClick={() => props.createLane({ name: 'New lane' })}>Add lane</button>
+    <Lanes lanes={props.lanes} />
+  </div>
+);
+
+Kanban.need = [() => { return fetchLanes(); }];
+
+Kanban.propTypes = {
+  lanes: PropTypes.array,
+  createLane: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+  lanes: Object.values(state.lanes),
+});
+
+const mapDispatchToProps = {
+  createLane: createLaneRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Kanban);
